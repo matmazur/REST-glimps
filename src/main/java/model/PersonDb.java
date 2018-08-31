9a5242c28bd9492742a9dc8afbbccd1859d2984c
mateusz mazur <matmazur90@gmail.com>
2018-08-31 17:45:51 +0200

    filled PersonsDB class holding data about people in map

--- src/main/java/model/PersonDb.java
+++ src/main/java/model/PersonDb.java
@@ -1,8 +1,42 @@
 package model;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PersonDb {
 
 
+private static Map<Long,Person> persons;
+
+public static Map<Long,Person> getPersons (){
+    if (persons==null){
+        persons=new HashMap<>();
+        persons.put(1L, new Person(1L, "Jack", "Black"));
+        persons.put(2L, new Person(2L, "Rick", "Sanchez"));
+        persons.put(3L, new Person(3L, "Peter", "Pan"));
+    }
+
+    return persons;
+}
+
+public void addPerson(Person person){
+
+   if (persons==null){
+       getPersons();
+   }
+    persons.put(person.getId(), person);
+
+}
+
+
+public Person remove (long id){
+    if (persons==null){
+        getPersons();
+    }
+
+    return persons.remove(id);
+
+}
 
 
 
